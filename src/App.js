42c7dc61d45102d9678e5b82aa3d60bcbdf639f4
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-06-05 17:50:14 +0800

    Init Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './reset.css'
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+     <h3 className='Bold'>Twitter</h3>
+    </div>
+  )
+}
+
+export default App
