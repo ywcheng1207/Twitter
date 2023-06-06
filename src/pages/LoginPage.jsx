f89dd87cee36f667cb94aa232f73badebf3a375f
rayray820206 <rayray820206@gmail.com>
2023-06-07 00:18:42 +0800

    feat: add LoginPage && SignUpPage

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,70 @@
+import DefaultInputItem from 'component/element/element_basic/DefaultInputItem/DefaultInputItem'
+import Button from 'component/element/element_basic/Button/Button'
+import styles from './LoginPage.module.scss'
+import { ReactComponent as Logo } from 'assets/icons/logo.svg'
+import { useState } from 'react'
+import { login } from '../api/auth'
+
+const LoginPage = () => {
+  const [account, setAccount] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleClick = async () => {
+    if (account.length === 0 || password.length === 0) {
+      return
+    }
+    const { success, token } = await login({ account, password })
+    if (success) {
+      console.log('登入成功')
+      console.log(token)
+    } else {
+      console.log('登入失敗')
+    }
+  }
+
+  return (
+        <div className={styles.registerContainer}>
+          <div className={styles.logoContainer}>
+            <Logo />
+          </div>
+          <div className={styles.title}>
+            <h3 className='Bold'>
+             登入 Alphitter
+            </h3>
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'帳號'}
+              placeholder={'請輸入帳號'}
+              value={account}
+              onChange={(value) => {
+                setAccount(value)
+              }}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'密碼'}
+              placeholder={'請輸入密碼'}
+              value={password}
+              type={password}
+              onChange={(value) => {
+                setPassword(value)
+              }}
+            />
+          </div>
+
+          <div className={styles.buttonContainer} onClick={handleClick}>
+            <Button type={'fullPill'} value={'登入'} />
+          </div>
+          <div className={styles.notice}>
+            <a href="" className={styles.register}>註冊</a>
+            <p>．</p>
+            <a href="" className={styles.backend}>後台登入</a>
+          </div>
+
+        </div>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,97 @@
+import DefaultInputItem from 'component/element/element_basic/DefaultInputItem/DefaultInputItem'
+import Button from 'component/element/element_basic/Button/Button'
+import styles from './SignUpPage.module.scss'
+import { ReactComponent as Logo } from 'assets/icons/logo.svg'
+import { useState } from 'react'
+import { register } from '../api/auth'
+
+const SignUpPage = () => {
+  const [account, setAccount] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [checkPassword, setCheckPassword] = useState('')
+
+  const handleClick = async () => {
+    if (account.length === 0 || name.length === 0 || email.length === 0 || password.length === 0 || checkPassword.length === 0) {
+      return
+    }
+    const { success } = await register({ account, name, email, password, checkPassword })
+    if (success) {
+      console.log('註冊成功')
+    } else {
+      console.log('註冊失敗')
+    }
+  }
+
+  return (
+        <div className={styles.registerContainer}>
+          <div className={styles.logoContainer}>
+            <Logo />
+          </div>
+          <div className={styles.title}>
+            <h3 className='Bold'>
+             建立你的帳號
+            </h3>
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'帳號'}
+              placeholder={'請輸入帳號'}
+              value={account}
+              onChange={(value) => {
+                setAccount(value)
+              }}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'名稱'}
+              placeholder={'請輸入使用者名稱'}
+              value={name}
+              onChange={(value) => {
+                setName(value)
+              }}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'Email'}
+              placeholder={'請輸入 Email'}
+              value={email}
+              onChange={(value) => {
+                setEmail(value)
+              }}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'密碼'}
+              placeholder={'請設定密碼'}
+              type={'password'}
+              value={password}
+              onChange={(value) => {
+                setPassword(value)
+              }}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <DefaultInputItem
+              label={'密碼確認'}
+              placeholder={'請再次輸入密碼'}
+              type={'password'}
+              value={checkPassword}
+              onChange={(value) => {
+                setCheckPassword(value)
+              }}
+            />
+          </div>
+          <div className={styles.buttonContainer} onClick={handleClick}>
+            <Button type={'fullPill'} value={'註冊'} />
+          </div>
+          <a href="" className={styles.cancel}>取消</a>
+        </div>
+  )
+}
+
+export default SignUpPage
