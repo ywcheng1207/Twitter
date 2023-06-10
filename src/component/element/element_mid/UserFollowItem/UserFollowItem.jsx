284ddeb51ca5234cd1aacbfd439d5eaa0e7b9362
rayray820206 <rayray820206@gmail.com>
2023-06-10 20:19:56 +0800

    feat: user: add Follower element-mid: add UserFollowItem

---
+++ src/component/element/element_mid/UserFollowItem/UserFollowItem.jsx
@@ -0,0 +1,32 @@
+import styles from './UserFollowItem.module.scss'
+import Button from 'component/element/element_basic/Button/Button'
+import clsx from 'clsx'
+
+const UserFollowItem = ({ item, onClick }) => {
+  const { followItemContainer, avatar, itemContent, nameAndBtn, name, btnContainer, tweet, follow } = styles
+  const isFollowed = item.isFollowed
+
+  return (
+        <div className={followItemContainer}>
+            <img src={item.avatar} alt="avatar" className={avatar} />
+            <div className={itemContent}>
+                <div className={nameAndBtn}>
+                    <p className={name}>{item.name}</p>
+                    <div className={clsx(btnContainer, { [follow]: isFollowed })} onClick={() => {
+                      onClick?.(item.tweetId)
+                    }} >
+                        <Button
+                            value={ item.isFollowed ? '正在跟隨' : '跟隨'}
+                            type={ item.isFollowed ? 'fullPill' : 'holePill' }
+                        />
+                    </div>
+                </div>
+                <div className={tweet}>
+                    {item.introduction}
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default UserFollowItem
--- src/component/user/Follow/Follower.jsx
+++ src/component/user/Follow/Follower.jsx
@@ -1,6 +1,96 @@
+import UserFollowItem from 'component/element/element_mid/UserFollowItem/UserFollowItem'
+import styles from './Follow.module.scss'
+import { ReactComponent as LeftArrow } from 'assets/icons/leftArrow.svg'
+import TweetSwitchTab from 'component/element/element_basic/TweetSwitchTab/TweetSwitchTab'
+import { useState } from 'react'
+
+const dummyData = [
+  {
+    tweetId: 7,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    isFollowed: true
+  },
+  {
+    tweetId: 1,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdf',
+    isFollowed: false
+  },
+  {
+    tweetId: 2,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdfaaaaaaaaaaaa',
+    isFollowed: true
+  },
+  {
+    tweetId: 3,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdf',
+    isFollowed: false
+  },
+  {
+    tweetId: 4,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdf',
+    isFollowed: false
+  },
+  {
+    tweetId: 5,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdf',
+    isFollowed: false
+  },
+  {
+    tweetId: 6,
+    name: 'ddd fie',
+    avatar: 'https://loremflickr.com/639/200/mountain/?random=61.42141615044277',
+    introduction: 'asdfasdfasdfasdfasdfasdf',
+    isFollowed: false
+  }
+]
+
 const Follower = () => {
+  const { container, headerText, tweetsCount, headerName } = styles
+  const [data, setData] = useState(dummyData)
+
+  const handleClick = (id) => {
+    setData(data.map(item => {
+      if (item.tweetId === id) {
+        return {
+          ...item,
+          isFollowed: !item.isFollowed
+        }
+      } else {
+        return item
+      }
+    }))
+  }
+
   return (
-    <div>Follower</div>
+    <div className={container}>
+      <header>
+        <LeftArrow />
+        <div className={headerText}>
+        <h4 className={headerName}>john dopw</h4>
+        <span className={tweetsCount}>25 推文</span>
+        </div>
+      </header>
+      <TweetSwitchTab
+        list={['追隨者', '正在追隨']}
+      />
+      { data.map(item => {
+        return (
+          <UserFollowItem key={item.tweetId} item={item} onClick={handleClick} />
+        )
+      })}
+    </div>
   )
 }
 
