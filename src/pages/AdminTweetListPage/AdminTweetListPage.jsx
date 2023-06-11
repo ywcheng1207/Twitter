9037c4390f7677d15e88b0da8612c2bd66c0beec
rayray820206 <rayray820206@gmail.com>
2023-06-12 01:50:13 +0800

    feat: add backend TweetListCard API integration

--- src/pages/AdminTweetListPage/AdminTweetListPage.jsx
+++ src/pages/AdminTweetListPage/AdminTweetListPage.jsx
@@ -1,222 +1,29 @@
 import styles from './AdminTweetListPage.module.scss'
 import AdminNav from 'component/element/element_basic/AdminNav/AdminNav'
 import TweetListCard from 'component/admin/TweetListCard/TweetListCard'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-const dummyData = [
-  {
-    tweetId: 51,
-    description: 'Minima reiciendis vel quo tempora sed architecto aut. Dolor quos consequuntur beatae quod blanditiis. Labore non repellendus officia. Et dol',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  },
-  {
-    tweetId: 52,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 53,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 54,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 55,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 56,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 57,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 58,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 59,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 60,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 61,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 62,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 663,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 64,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 65,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 66,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }, {
-    tweetId: 67,
-    description: 'Quia delectus fuga officia ea quis a occaecati possimus. Nostrum autem hic consequatur ut. Ad assumenda dolore assumenda veniam qui. Tempore',
-    userId: {
-      id: 8,
-      name: 'user1',
-      avatar: 'https://loremflickr.com/320/240/people/?random=7.976051090916998',
-      account: 'user1'
-    },
-    tweetTime: '2023-06-06T18:02:16.000Z',
-    replyCount: 3,
-    likeCount: 0
-  }
-]
+import { getTweets } from 'api/user'
 
 const AdminTweetListPage = () => {
-  const [data, setData] = useState(dummyData)
+  const [tweetList, setTweetList] = useState([])
   const [status, setStatus] = useState('tweetList')
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const getUserDataAsync = async (authToken) => {
+      try {
+        const data = await getTweets(authToken)
+        console.log(data[0])
+        setTweetList(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    if (localStorage.getItem('authToken')) {
+      getUserDataAsync(localStorage.getItem('authToken'))
+    }
+  }, [])
 
   const handleTweetListClick = () => {
     setStatus('tweetList')
@@ -231,7 +38,7 @@ const AdminTweetListPage = () => {
   }
 
   const handleDelete = (id) => {
-    setData(data.filter(item => item.tweetId !== id))
+    setTweetList(tweetList.filter(item => item.tweetId !== id))
   }
 
   const { container, mainContainer, tweetListContainer } = styles
@@ -249,7 +56,7 @@ const AdminTweetListPage = () => {
                     <h4 className='Bold'>推文清單</h4>
                 </header>
                 <div className={tweetListContainer}>
-                    <TweetListCard data={data} onDelete={handleDelete}/>
+                    <TweetListCard data={tweetList} onDelete={handleDelete}/>
                 </div>
             </div>
         </div>
