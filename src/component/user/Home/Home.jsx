72334cb0b2748059c8d29b74e96586ebd4f43002
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-06-12 01:14:41 +0800

    feat: add hom page api integration

--- src/component/user/Home/Home.jsx
+++ src/component/user/Home/Home.jsx
@@ -1,33 +1,34 @@
 import styles from './Home.module.scss'
 import HomeContentHead from 'component/element/element_mid/HomeContentHead/HomeContentHead'
 import HomeContentItem from 'component/element/element_mid/HomeContentItem/HomeContentItem'
-import homepageDummy from 'dummyData/homepageDummy'
-// import { useEffect } from 'react'
-// import { getTweets } from 'api/user'
+// import homepageDummy from 'dummyData/homepageDummy'
+import { useEffect, useState } from 'react'
+import { getTweets } from 'api/user'
 
 const Home = () => {
   const { contentContainer, HomeContentItemList } = styles
+  const [homeList, setHomeList] = useState([])
 
-  // useEffect(() => {
-  //   const getUserDataAsync = async (authToken) => {
-  //     try {
-  //       const data = await getTweets(authToken)
-  //       console.log(data)
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
-  //   console.log(localStorage.getItem('authToken'))
-  //   if (localStorage.getItem('authToken')) {
-  //     getUserDataAsync(localStorage.getItem('authToken'))
-  //   }
-  // }, [])
+  useEffect(() => {
+    const getUserDataAsync = async (authToken) => {
+      try {
+        const data = await getTweets(authToken)
+        console.log(data[0])
+        setHomeList(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    if (localStorage.getItem('authToken')) {
+      getUserDataAsync(localStorage.getItem('authToken'))
+    }
+  }, [])
 
   return (
     <div className={contentContainer}>
       <HomeContentHead />
         <div className={HomeContentItemList}>
-           {homepageDummy.map((item) => (
+           {homeList.map((item) => (
               <HomeContentItem tweet={item} key={item.tweetId} />
            ))}
         </div>
