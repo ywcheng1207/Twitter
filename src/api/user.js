1d5e2ce68d30ec58494377c736933522627a907c
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-06-12 15:02:13 +0800

    style: modify home page console message

--- src/api/user.js
+++ src/api/user.js
@@ -11,12 +11,25 @@ export const getTweets = async (authToken) => {
   }
 }
 
-export const createTweets = async (payload) => {
-  const { description } = payload
+// export const createTweets = async (payload) => {
+//   const { description } = payload
+//   try {
+//     const res = await axios.post(`${baseURL}/tweets`, { description })
+//     return res.data
+//   } catch (error) {
+//     console.error('[Create Tweets failed]: ', error)
+//   }
+// }
+
+// -- 個人資料頁底下的推文串
+export const getUserTweets = async (authToken, id) => {
   try {
-    const res = await axios.post(`${baseURL}/tweets`, { description })
+    const res = await axios.get(`${baseURL}/users/${id}/tweets `, { headers: { Authorization: 'Bearer ' + authToken } })
     return res.data
   } catch (error) {
-    console.error('[Create Tweets failed]: ', error)
+    console.error('[Get Tweets failed]', error)
   }
 }
+// -- 個人資料頁底下的回覆串
+
+// -- 個人資料頁底下的喜歡串
