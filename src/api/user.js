3e6b3a8ce97df32c037b9216d0db46d2a70ebc36
rayray820206 <rayray820206@gmail.com>
2023-06-13 01:42:18 +0800

    feat:add Other layout , add infoSetting & PersonalInfo api (PersonalInfoModal doesn't complete)

--- src/api/user.js
+++ src/api/user.js
@@ -48,8 +48,24 @@ export const getUserLikeTweets = async (authToken, id) => {
     console.error('[Get Tweets failed]', error)
   }
 }
+// -- 設定頁 post 更改使用者帳號資料
+export const putAccountInfo = async (authToken, id, userInfo) => {
+  try {
+    const { account, name, email, password, checkPassword } = userInfo
+    const res = await axios.post(`${baseURL}/users/${id}`, {
+      account,
+      name,
+      email,
+      password,
+      checkPassword
+    }, { headers: { Authorization: 'Bearer ' + authToken } })
+    return res.data
+  } catch (error) {
+    console.error('[postAccountInfo failed]', error)
+  }
+}
 
-// -- 設定頁取得使用者 帳號 密碼 Email
+// -- 許多頁取得使用者資料
 export const getAccountInfo = async (authToken, id) => {
   try {
     const { data } = await axios.get(`${baseURL}/users/${id}`, { headers: { Authorization: 'Bearer ' + authToken } })
@@ -58,3 +74,31 @@ export const getAccountInfo = async (authToken, id) => {
     console.error('[getAccountInfo failed]', error)
   }
 }
+
+// // -- Other 頁底下的推文串
+// export const getOtherTweets = async (authToken, id) => {
+//   try {
+//     const res = await axios.get(`${baseURL}/users/${id}/tweets `, { headers: { Authorization: 'Bearer ' + authToken } })
+//     return res.data
+//   } catch (error) {
+//     console.error('[Get OtherTweets failed]', error)
+//   }
+// }
+// // -- Other 頁底下的回覆串
+// export const getOtherReplyTweets = async (authToken, id) => {
+//   try {
+//     const res = await axios.get(`${baseURL}/users/${id}/replied_tweets `, { headers: { Authorization: 'Bearer ' + authToken } })
+//     return res.data
+//   } catch (error) {
+//     console.error('[Get OtherTweets failed]', error)
+//   }
+// }
+// // -- Other 頁底下的喜歡串
+// export const getOtherLikeTweets = async (authToken, id) => {
+//   try {
+//     const res = await axios.get(`${baseURL}/users/${id}/likes `, { headers: { Authorization: 'Bearer ' + authToken } })
+//     return res.data
+//   } catch (error) {
+//     console.error('[Get OtherTweets failed]', error)
+//   }
+// }
