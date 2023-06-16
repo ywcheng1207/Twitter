cb718ba328cbb9e31295e9b01359d92b8920a131
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-06-16 13:17:17 +0800

    fix: modify api

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const baseURL = 'https://arcane-mesa-58606.herokuapp.com/api'
-// const baseURL = 'https://rocky-reef-54442.herokuapp.com/api'
+// const baseURL = 'https://arcane-mesa-58606.herokuapp.com/api'
+const baseURL = 'https://rocky-reef-54442.herokuapp.com/api'
 
 export const getTweets = async (authToken) => {
   try {
